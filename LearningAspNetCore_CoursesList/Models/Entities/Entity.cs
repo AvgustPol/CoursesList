1ac9275dbb3aa194e0f59ca1046d82c0127b09db
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2019-02-20 22:32:46 +0100

    Added Courses Model

---
+++ LearningAspNetCore_CoursesList/Models/Entities/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningAspNetCore_CoursesList.Models.Entities
+{
+    /// <summary>
+    /// Just class for creating an abstraction
+    /// </summary>
+    public abstract class Entity
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
