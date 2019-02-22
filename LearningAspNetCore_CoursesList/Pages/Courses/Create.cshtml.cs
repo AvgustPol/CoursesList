a7f9ade8171b957f9e520b45559e8b494601141b
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2019-02-22 12:52:52 +0100

    Added to create page form first input

--- LearningAspNetCore_CoursesList/Pages/Courses/Create.cshtml.cs
+++ LearningAspNetCore_CoursesList/Pages/Courses/Create.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LearningAspNetCore_CoursesList.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +10,11 @@ namespace LearningAspNetCore_CoursesList.Pages.Courses
 {
     public class CreateModel : PageModel
     {
+        [BindProperty]
+        public Course Course { get; set; }
+
         public void OnGet()
         {
-
         }
     }
 }
