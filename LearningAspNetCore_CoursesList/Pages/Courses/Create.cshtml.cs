5eff831bda1af2f3439a614d07af9d1ef8e7eff0
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2019-02-22 15:51:13 +0100

    Update Create.cshtml.cs
    Added post handler

--- LearningAspNetCore_CoursesList/Pages/Courses/Create.cshtml.cs
+++ LearningAspNetCore_CoursesList/Pages/Courses/Create.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LearningAspNetCore_CoursesList.Models;
 using LearningAspNetCore_CoursesList.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,11 +11,30 @@ namespace LearningAspNetCore_CoursesList.Pages.Courses
 {
     public class CreateModel : PageModel
     {
+        private readonly MyApplicationDbContext _dbContext;
+
+        public CreateModel(MyApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         [BindProperty]
         public Course Course { get; set; }
 
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _dbContext.Courses.AddAsync(Course);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToPage("Index");
         }
     }
 }
