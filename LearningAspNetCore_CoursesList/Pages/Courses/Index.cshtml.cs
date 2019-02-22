3d63605b14b64d617c05d2fa3faf91e9426c11c9
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2019-02-22 16:10:02 +0100

    Update Create
    Added OnGet logic to get courses from the database

--- LearningAspNetCore_CoursesList/Pages/Courses/Index.cshtml.cs
+++ LearningAspNetCore_CoursesList/Pages/Courses/Index.cshtml.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LearningAspNetCore_CoursesList.Models;
+using LearningAspNetCore_CoursesList.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace LearningAspNetCore_CoursesList.Pages.Courses
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly MyApplicationDbContext _dbContext;
 
+        public IEnumerable<Course> Courses { get; set; }
+
+        public IndexModel(MyApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task OnGet()
+        {
+            Courses = await _dbContext.Courses.ToListAsync();
         }
     }
 }
