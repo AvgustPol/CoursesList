bcd21fc9d4ef49d7855d4fcc5a07f628915c4872
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2019-02-24 11:44:40 +0100

    Added Edit Get Handler

---
+++ LearningAspNetCore_CoursesList/Pages/Courses/Edit.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearningAspNetCore_CoursesList.Models;
+using LearningAspNetCore_CoursesList.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LearningAspNetCore_CoursesList.Pages.Courses
+{
+    public class EditModel : PageModel
+    {
+        private readonly MyApplicationDbContext _dbContext;
+
+        public EditModel(MyApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [BindProperty]
+        public Course Course { get; set; }
+
+        public void OnGet(int id)
+        {
+            Course = _dbContext.Courses.Find(id);
+        }
+    }
+}
